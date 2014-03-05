04c94b98214d221cb76212659890c894690f75a8
Johannes Wachter <johannes.wachter@students.fhv.at>
2014-03-05 11:45:24 +0100

    added basic interface

--- events.h
+++ events.h
@@ -1,8 +1,21 @@
 /*
- * Event-Manager: events.h
+ * IPC: events.h
  * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
  *
  * Created on: 05.03.2014
  * Description:
  * TODO
  */
+#ifndef _EVENTS_H_
+#define _EVENTS_H_
+#include<stdbool.h>
+#include<inttypes.h>
+
+// TODO remove after include scheduler.h
+typedef int8_t processID;
+
+bool events_on(char event_name[], processID pid);
+
+bool events_emit(char event_name[], processID pid);
+
+#endif
